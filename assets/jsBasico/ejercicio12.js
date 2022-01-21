705187bf1bb588b23d0ccdaf059d1326b1d20ec6
felipe131305 <felipe08119617@gmail.com>
2022-01-21 11:15:35 -0500

    se agrego log en el ejercicio12.js

--- assets/jsBasico/ejercicio12.js
+++ assets/jsBasico/ejercicio12.js
@@ -5,6 +5,7 @@ let nombreUsuario = document.getElementById("nombreUsuario");
 const cambiarNombre = () => {
   let nombre = prompt("Ingresa tu nombre");
   nombreUsuario.innerHTML = "Bienvenido :" + nombre;
+  console.log(nombre)
 };
 
 // Eventos siempre al final
